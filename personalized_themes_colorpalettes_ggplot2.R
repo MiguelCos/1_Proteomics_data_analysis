8e02c41dfee73a1484481f0e9644273f55d831ad
Miguel <migueljcc5@gmail.com>
2020-10-26 13:42:06 +0100

    color pallete and customized theme

---
+++ personalized_themes_colorpalettes_ggplot2.R
@@ -0,0 +1,8 @@
+## Personalized themes and color pallettes ----
+
+scale_fill_manual(values=c("#071d5c", "#008683", "#005583", "#84b082"))+
+          theme(axis.text.x = element_text(hjust = 0.5, vjust = 0.1, size = 10),
+                panel.background = element_blank(),
+                panel.grid.major = element_line(color = "grey"),
+                panel.border = element_rect(colour = "black", fill=NA, size=1.5),
+                axis.title=element_text(size=12,face="bold"))
