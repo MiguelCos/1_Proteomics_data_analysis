f8b6038ebcd2ac96a1d850bfdf840601deae2bff
Miguel <migueljcc5@gmail.com>
2021-03-12 10:35:14 +0100

    add option to input the values by their 1/X of the minimal intensity observed by protein

--- filter_nas.R
+++ filter_nas.R
@@ -3,7 +3,7 @@
 ### This is a guidance script/function to filter out those proteins with X
 # max missing values in a particular group.
 
-na_filter <- function(data, max_na_per_group, logtrans){
+na_filter <- function(data, max_na_per_group, logtrans, minInt_inpute, fraction_Inpute = 5){
           
           data_long <- pivot_longer(data = data,
                                     cols = matches("^S"),
@@ -14,7 +14,8 @@ na_filter <- function(data, max_na_per_group, logtrans){
                              sep = "\\-")
           
           if (logtrans){
-                    data_long <- mutate(data_long, Intensity = log2(Intensity))
+                    data_long <- mutate(data_long,
+                                        Intensity = log2(Intensity))
           }
           
           na_count <- group_by(data_long,
@@ -33,13 +34,37 @@ na_filter <- function(data, max_na_per_group, logtrans){
           included_prots <- dplyr::filter(nafraction,
                                           max_nafraction <= max_na_per_group)
           
-          data_long_nonas1 <- filter(data_long,
-                                     ID %in% included_prots$ID) %>% 
-                    mutate(`Group-Sample` = paste(Group,"-",Sample, sep = "")) %>%
-                    dplyr::select(ID, `Group-Sample`, Intensity)
           
-          data_maxperc_nas <- pivot_wider(data_long_nonas1,
-                                          names_from = `Group-Sample`,
-                                          values_from = Intensity)
+          
+          if (minInt_inpute){
+                    
+                    min_intensityprot <- data_long %>% 
+                              group_by(ID) %>% 
+                              mutate(min_int = min(Intensity, na.rm = TRUE)) %>%
+                              mutate(th_of_min_int = min_int/fraction_Inpute)
+                    
+                    data_long_nonas1 <- min_intensityprot %>% 
+                              mutate(Intensity = ifelse(is.na(Intensity),
+                                                        yes = th_of_min_int,
+                                                        no = Intensity),
+                                     `Group-Sample` = paste(Group,"-",Sample, sep = "")) %>%
+                              dplyr::select(ID, `Group-Sample`, Intensity) %>%
+                              dplyr::filter(ID %in% included_prots$ID)
+                    
+                    data_maxperc_nas <- pivot_wider(data_long_nonas1,
+                                                    names_from = `Group-Sample`,
+                                                    values_from = Intensity)
+          } else {
+                    data_long_nonas1 <- filter(data_long,
+                                               ID %in% included_prots$ID) %>% 
+                              mutate(`Group-Sample` = paste(Group,"-",Sample, sep = "")) %>%
+                              dplyr::select(ID, `Group-Sample`, Intensity)
+                    
+                    data_maxperc_nas <- pivot_wider(data_long_nonas1,
+                                                    names_from = `Group-Sample`,
+                                                    values_from = Intensity)
+          }
+          
+          
 }
 
